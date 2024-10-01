2b9d85b441664547576475101faa8ca25574c63f
dayeone1 <s2312@e-mirim.hs.kr>
2024-10-01 10:32:22 +0900

    창 설정

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+# Pygame 초기화
+pygame.init()
+
+# 창 크기 설정
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Pygame 프로젝트')
+
